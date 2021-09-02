7ace19214bc1846c53bd71e829cf53c8c2b7ac2a
daviddpi <piscopodavid97@gmail.com>
2021-09-02 15:20:10 +0200

    aggiunto livelli di difficolta

--- js/myscript.js
+++ js/myscript.js
@@ -5,21 +5,63 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min) + min); //Massimo escluso, minimo incluso
 }
 
-const numeroBombe = 16;
-let bombe = [];
+//funzione che verifica la difficoltà
+function verificaDifficolta(strDifficolta){
+    if( (strDifficolta != "pari") && (strDifficolta != "media") && (strDifficolta != "difficile") ){
+        return true;
+    }
+    return false;
+}
 
-// while(bombe.length < numeroBombe){
-//     var randomNum = getRandomInt(1,101);
-//     //se non è presente l'elemento nell'array(===-1) allora lo inserisce
-//     if(bombe.indexOf(randomNum) === -1){
-//         bombe.push(randomNum);
-//     }
-//     console.log(randomNum);
-// }
+//variabili inizializzate
+let difficolta;
+let numeroBombe;
+let bombe = [];
+let tuoNumero = [];
+let score = 0;
+let numeroMassimo;
+let livello;
+
+//ciclo per scelgliere una difficolta
+do{
+    difficolta = prompt("Seleziona la difficoltà:\n-pari-\n-media-\n-difficile-");
+    if(difficolta != difficolta.trim().toLowerCase()){
+        difficolta = difficolta.trim().toLowerCase();
+    }
+    if( verificaDifficolta(difficolta) == true ){
+        alert("Devi inserire una difficolta esistente");
+    }
+
+}while( verificaDifficolta(difficolta) );
+
+console.log(difficolta);
+
+//in base alla difficolta cambiano il numero delle bombe e il numero totale che l'utente dovrà inserire
+switch(difficolta){
+    case "pari":
+        numeroBombe = 2;
+        numeroMassimo = 10;
+        break;
+    case "media":
+        numeroBombe = 6;
+        numeroMassimo = 25;
+        break;
+    case "difficile":
+        numeroBombe = 10;
+        numeroMassimo = 40;
+        break;
+    default:
+        numeroBombe = 16;
+        numeroMassimo = 100;
+        break;
+}
+
+//livello ha adesso un valore
+livello = numeroMassimo - numeroBombe
 
 //algoritmo che inserisce univocamente un numero random da 1 a 100
 do{
-    let randomNum = getRandomInt(1,101)
+    let randomNum = getRandomInt(1,numeroMassimo+1)
     if(!(bombe.includes(randomNum))){ //se il numero random non è già nell'array viene inserito
         bombe.push(randomNum);
     }
@@ -28,32 +70,29 @@ do{
 
 console.log(bombe);
 
-let tuoNumero = [];
-let score = 0;
-
 //ciclo in cui l'utente inserisce un numero < 100 e > 1 che se ripetuto interrompe il ciclo
 do{
-    let numeroAttuale = parseInt ( prompt("Inserisci un numero") );
-    if(numeroAttuale < 1 || numeroAttuale > 100 || Number.isNaN(numeroAttuale)){
-        alert("Inserisci un numero da 1 a 100");
+    let numeroAttuale = parseInt ( prompt("Inserisci un numero da 1 a " + numeroMassimo) );
+    if(numeroAttuale < 1 || numeroAttuale > numeroMassimo || Number.isNaN(numeroAttuale)){
+        alert("Devi inserire un numero da 1 a " + numeroMassimo +"!");
     } else if(tuoNumero.includes(numeroAttuale)){ //numero già inserito in precedenza
-        alert("Hai già inserito questo numero!\nHai perso!\n\nScore: " + score);
-        break;
+        alert("Hai già inserito questo numero! " + numeroAttuale);
     } else if(bombe.includes(numeroAttuale)){ //caso in cui esiste un numero nell'array delle bombe
         alert("HAI PERSO!\nHai preso la bomba!\n\nScore: " + score);
         break;
     } else {
         tuoNumero.push(numeroAttuale);
-        score++;
+        score++;                     
     }
-
+    
     console.log("Numero inserito: " + numeroAttuale);
     console.log(" array: " + tuoNumero);
     console.log(" punteggio: " + score);
     console.log(tuoNumero.length);
+    console.log(livello);
 
-    if(tuoNumero.length == (100 - numeroBombe)){
+    if(tuoNumero.length == livello){
         alert("HAI VINTO!\nScore: " + score);
     }
 
-} while( ((tuoNumero >= 1 || tuoNumero <= 100) && ( !(Number.isNaN(tuoNumero)) ) ) || (tuoNumero.length < (100 - numeroBombe)) );
+}while( ((tuoNumero >= 1 || tuoNumero <= numeroMassimo) && ( !(Number.isNaN(tuoNumero)) ) ) || (tuoNumero.length < livello) );
